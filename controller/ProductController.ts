ec22fe11481bd7503f93b7d4662aef8920958a09
pphatt <vutienphat0809@gmail.com>
2023-07-01 11:54:13 +0700

    feat: add filter for search product

--- controller/ProductController.ts
+++ controller/ProductController.ts
@@ -5,9 +5,20 @@ import expressAsyncHandler from "express-async-handler";
 // @route GET /product
 // @access Private
 const getProducts = expressAsyncHandler(async (req, res) => {
-  const page = req.query.page as string
-  const products = await Product.find().limit(parseInt(page) * 4).skip((parseInt(page) - 1) * 4).lean();
-  console.log(req.query)
+  const page = req.query.page as string;
+  const queryName = req.query.search as string;
+  const size = await Product.count();
+
+  let find = {}
+
+  if (queryName !== undefined && queryName !== "") {
+    find = { name: {$regex: queryName} }
+  }
+
+  const products = await Product.find(find)
+    .limit(10)
+    .skip((parseInt(page) - 1) * 10)
+    .lean();
 
   if (!products.length) {
     res.status(400).json({ message: "No products was found" });
@@ -24,21 +35,30 @@ const addProduct = expressAsyncHandler(async (req, res) => {
   const { name, price, manufacturer, scale, image, state } = req.body;
 
   if (!name || !price || !manufacturer || !scale || !image || !state) {
-    res.status(404).json({ message: "All fields are required"})
-    return
+    res.status(404).json({ message: "All fields are required" });
+    return;
   }
 
   const product = await Product.findOne({ productName: name }).lean().exec();
 
   if (product?.name === name) {
-    res.status(400).json({ message: "Product already exists"})
-    return
+    res.status(400).json({ message: "Product already exists" });
+    return;
   }
 
-  const add = await Product.create({ name, price, image, manufacturer, scale, state });
+  const add = await Product.create({
+    name,
+    price,
+    image,
+    manufacturer,
+    scale,
+    state,
+  });
 
   if (add) {
-    res.status(201).json({ message: `New product ${name} was added`, _id: add._id });
+    res
+      .status(201)
+      .json({ message: `New product ${name} was added`, _id: add._id });
   } else {
     res.status(400).json({ message: "Invalid product data received" });
   }
